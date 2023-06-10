2c0729c829474397c52428c3a66cc437e4cb41f5
Empyreal96 <64583248+Empyreal96@users.noreply.github.com>
2023-06-10 20:58:20 +0100

    Small update mainly to test new plugin and UWP client release

---
+++ js/plugins/rpg_savesmanager.js
@@ -0,0 +1,140 @@
+/*:
+ * @plugindesc rpg_savesmanager.js - A RPG Maker localStorage manager.
+ * @author Bashar Astifan (License: MIT)
+ *
+ * @help This plugin adds 3 new Main Menu items that allow
+ * Backup/Restore and Delete of Save Data, Target for UWP environment
+ */
+
+
+/*******************/
+/* DISABLE ERRORS  */
+/*******************/
+console.error = function (...args) {
+  //Generate warning instead
+  console.warn(args);
+}
+
+//Backup file type
+var backupFileType = "bak";
+
+/*******************/
+/* SAVE MANAGEMENT */
+/*******************/
+function getLocalStorage() {
+  return JSON.stringify(localStorage);
+}
+
+function writeLocalStorage(data) {
+  var dataJSON = JSON.parse(data);
+  localStorage.clear();
+  Object.keys(dataJSON).forEach(function (key) {
+    localStorage.setItem(key, dataJSON[key]);
+  });
+}
+
+
+/* SAVE FILE SECTION */
+//Append hidden iframe element
+var iframe = document.createElement("iframe");
+iframe.style.display = "none";
+document.body.appendChild(iframe);
+iframe.onload = function () {
+  var iframeWindow = iframe.contentWindow;
+  var iframeDocument = iframeWindow.document;
+  iframe.contentWindow["dataSaver"] = function (data, fileName) {
+    iframeDocument.title = fileName;
+    var textToSaveAsBlob = new Blob([data], {
+      type: "text/plain"
+    });
+    var textToSaveAsURL = window.URL.createObjectURL(textToSaveAsBlob);
+    iframeWindow.location.replace(textToSaveAsURL);
+  };
+};
+
+function saveLocalStorage() {
+  /* dump local storage to string */
+  /* save as blob */
+  var fileName = document.title.replace(/\s/g, "_") + "." + backupFileType;
+  var scriptCode = "getLocalStorage()";
+  window.location.replace("rpgbackup:" + fileName + "," + scriptCode);
+}
+
+
+/* IMPORT FILE SECTION */
+//Append hidden input element
+var importFile = document.createElement("input");
+importFile.type = "file";
+importFile.accept = backupFileType + "/*";
+importFile.style.display = "none";
+document.body.appendChild(importFile);
+
+//Attach input event
+importFile.addEventListener("change", function (e) {
+  if (e.target.files !== null && e.target.files.length > 0) {
+    var selectedFile = e.target.files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function () {
+      //File loaded
+      var fileContent = fileReader.result;
+      writeLocalStorage(fileContent);
+      SceneManager.goto(Scene_Title);
+    };
+    fileReader.onerror = function (error) {
+      //Failed to load
+      console.error(error);
+    };
+    fileReader.readAsText(selectedFile);
+  }
+});
+
+function restoreLocalStorage() {
+  /* import without button hack */
+  importFile.click();
+}
+
+
+/*******************/
+/* MENU MANAGEMENT */
+/*******************/
+
+//Save copy of original makeCommandList
+var tempMakeCommandList = Window_TitleCommand.prototype.makeCommandList;
+//Override the function
+Window_TitleCommand.prototype.makeCommandList = function () {
+  //Call original
+  tempMakeCommandList.call(this);
+  //Append our options
+  this.addCommand("Backup Saves", 'backupStorage', localStorage.length > 0);
+  this.addCommand("Restore Saves", 'restoreStorage');
+  this.addCommand("Reset", 'resetStorage');
+};
+
+//Save copy of original createCommandWindow
+var tempCreateCommandWindow = Scene_Title.prototype.createCommandWindow;
+Scene_Title.prototype.createCommandWindow = function () {
+  //Call original
+  tempCreateCommandWindow.call(this);
+  //Append our options
+  this._commandWindow.setHandler('backupStorage', this.commandBackupStorage.bind(this));
+  this._commandWindow.setHandler('restoreStorage', this.commandRestoreStorage.bind(this));
+  this._commandWindow.setHandler('resetStorage', this.commandResetStorage.bind(this));
+};
+
+Scene_Title.prototype.commandBackupStorage = function () {
+  saveLocalStorage();
+  this.fadeOutAll();
+  SceneManager.goto(Scene_Title);
+};
+
+Scene_Title.prototype.commandRestoreStorage = function () {
+  restoreLocalStorage();
+  this.fadeOutAll();
+  SceneManager.goto(Scene_Title);
+};
+
+Scene_Title.prototype.commandResetStorage = function () {
+  localStorage.clear();
+  this.fadeOutAll();
+  SceneManager.goto(Scene_Title);
+};
